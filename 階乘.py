8157323b2cd553fde8b465f8cf8a3d7b856a445f
XXXshaunXXX <78331055+XXXshaunXXX@users.noreply.github.com>
2021-02-01 17:04:15 +0800

    Add files via upload

---
+++ 階乘.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 15:45:12 2021
+
+@author: USER
+"""
+a = int(input())
+total = 1
+for a in range(a,0,-1):
+    total*=a
+print(total)
+    
+    
